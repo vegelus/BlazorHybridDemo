667c77ef3e37b6b48874d6525602c5c1ff3d9719
Pawel <vegelus@ow.pl>
2026-01-12 22:56:54 +0100

    Add wpf

---
+++ BlazorHybridDemo/BlazorHybridDemo.Wpf/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace BlazorHybridDemo.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var services = new ServiceCollection();      
+            services.AddWpfBlazorWebView();
+#if DEBUG
+            services.AddBlazorWebViewDeveloperTools();
+#endif
+            var serviceProvider = services.BuildServiceProvider();
+            this.Resources.Add("ServiceProvider", serviceProvider);
+        }
+    }
+
+}
